7266273bc7aa42c6c403a6db0b83a0dc95303dd6
Kevin Ying <yingk@berkeley.edu>
2025-11-03 12:52:14 -0800

    minor formatting

--- multi_channel_pmw/src/main.cpp
+++ multi_channel_pmw/src/main.cpp
@@ -5,28 +5,21 @@ MegaPhasePwm pwmDriver;
 
 void setup() {
     Serial.begin(115200);
-    while (!Serial); // Wait for serial connection
-    Serial.println("Debug: Opposite Wave Test");
-    
-    pwmDriver.begin(300.0);
+
+    pwmDriver.begin(300.0); // 300Hz
     Serial.println("Debug: PWM initialized at 300Hz");
 
     // Test with higher duty cycles to make waves more visible
-    pwmDriver.setDutyCycle(0, 0.4); // Channel 0 - 50% duty
-	pwmDriver.setDutyCycle(5, 0.5); // Channel 1 - 50% duty
-    pwmDriver.setDutyCycle(6, 0.6); // Channel 2 - 50% duty
+    pwmDriver.setDutyCycle(0, 0.5); // Channel 0 - 50% duty
+	pwmDriver.setDutyCycle(5, 0.5); // Channel 5 - 50% duty
+    pwmDriver.setDutyCycle(6, 0.5); // Channel 6 - 50% duty
     Serial.println("Debug: Duty cycles set");
 
 	pwmDriver.setPhase(0, 0.0);
-    pwmDriver.setPhase(6, 0.0 + 60);
-	pwmDriver.setPhase(5, 0.0 + 180);
-
-    
-    delay(1000); // Give time to see initial state
+    pwmDriver.setPhase(5, 0.0 + 60);
+	pwmDriver.setPhase(6, 0.0 + 180);
 }
 
-float phase = 0.0;
-
 void loop() {
-    delay(100); // Slower to observe
+    // delay(100); // Slower to observe
 }
